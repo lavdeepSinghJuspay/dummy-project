9d91fad39c76312b1cb0c697fe0ca7cd706a56df
lavdeepSinghJuspay <122526221+lavdeepSinghJuspay@users.noreply.github.com>
2023-11-05 02:30:22 +0530

    Create make_request.py

---
+++ make_request.py
@@ -0,0 +1,15 @@
+import requests
+
+url = 'http://localhost:8080/api'
+
+try:
+    response = requests.get(url)
+    if response.status_code == 200:
+        print('Request to Flask service succeeded.')
+        exit(0)
+    else:
+        print('Request to Flask service failed.')
+        exit(1)
+except requests.exceptions.RequestException as e:
+    print(f'Error making the request: {str(e)}')
+    exit(1)
