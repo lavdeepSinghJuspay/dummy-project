38d5ddee50a1d4b4fab0614981482449a047b6c6
lavdeepSinghJuspay <122526221+lavdeepSinghJuspay@users.noreply.github.com>
2023-11-05 01:47:47 +0530

    Create install_python.sh

---
+++ install_python.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Install Homebrew if not already installed
+if ! command -v brew &> /dev/null; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+# Update Homebrew
+brew update
+
+# Install Python 3
+brew install python@3
+
+# Verify the installation
+python3 --version
+pip3 --version
