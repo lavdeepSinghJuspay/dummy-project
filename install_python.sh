0a424b2c953268ded5af29484be1295d32d1229a
lavdeepSinghJuspay <122526221+lavdeepSinghJuspay@users.noreply.github.com>
2023-11-05 02:17:24 +0530

    Update install_python.sh

--- install_python.sh
+++ install_python.sh
@@ -3,16 +3,28 @@
 # Install Homebrew if not already installed
 if ! command -v brew &> /dev/null; then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# Check if brew is installed, if not, try to install Python using the system package manager
+
+elif [ -x "$(command -v brew)" ]; then
+    echo "Using Homebrew to install Python..."
+    brew install python
+else
+    echo "Using system package manager to install Python..."
+    if [ -x "$(command -v apt-get)" ]; then
+        sudo apt-get install python3
+    elif [ -x "$(command -v yum)" ]; then
+        sudo yum install python3
+    elif [ -x "$(command -v dnf)" ]; then
+        sudo dnf install python3
+    else
+        echo "System package manager not found or not supported. Please install Python manually."
+    fi
 fi
 
-# Update Homebrew
-brew update
-
-pip install -r requirements.txt
-
-# Install Python 3
-brew install python@3
-
-# Verify the installation
+# Verify Python and pip versions
 python3 --version
 pip3 --version
+
+# Install Python dependencies from requirements.txt
+pip3 install -r requirements.txt
+
